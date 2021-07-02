6ab580836fe6b562808866596ed053f012760f9b
Sheela G. Khusal <75377067+sgkhusal@users.noreply.github.com>
2021-07-02 17:26:25 -0300

    Updated ft_strjoin function according to corrections applied in "get next line" project in order to achieve better code performance.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,31 +12,23 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	size_t	size;
-	size_t	i;
-	size_t	j;
+	int		i;
+	int		j;
 	char	*join;
 
 	if (!s1 && !s2)
 		return (NULL);
-	if (!s1 || !s1[0])
-		return (ft_strdup((s2)));
-	if (!s2 || !s2[0])
-		return (ft_strdup(s1));
-	size = ft_strlen(s1) + ft_strlen(s2) + 1;
-	if (!(join = (char *)malloc(size * sizeof(char))))
+	join = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!join)
 		return (NULL);
-	i = 0;
-	while (i++ <= ft_strlen(s1) && s1[i - 1])
-		join[i - 1] = s1[i - 1];
-	j = 0;
-	while (i - 1 + j < size - 1)
-	{
-		join[i - 1 + j] = s2[j];
-		j++;
-	}
-	join[i - 1 + j] = '\0';
+	i = -1;
+	while (s1[++i])
+		join[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		join[i + j] = s2[j];
+	join[i + j] = '\0';
 	return (join);
 }
