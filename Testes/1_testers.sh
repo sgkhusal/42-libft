1610698fb980a846274e82221b99593516c2faa3
Sheela G. Khusal <75377067+sgkhusal@users.noreply.github.com>
2021-08-21 20:37:55 -0300

    Create 1_testers.sh
    Added script to external testers

---
+++ Testes/1_testers.sh
@@ -0,0 +1,25 @@
+# jtoty/Libftest
+git clone https://github.com/jtoty/Libftest.git
+cd Libftest
+bash grademe.sh
+#edit libft path in my_config.sh
+vim my_config.sh
+# :wq
+bash grademe.sh
+
+cd ..
+# alelievr/libft-unit-test
+git clone https://github.com/alelievr/libft-unit-test.git
+cd libft-unit-tests
+make
+
+cd ..
+# Tripouille/libftTester
+cd libft
+git clone https://github.com/Tripouille/libftTester.git
+cd libftTester
+make a
+# make m
+# make b
+
+
